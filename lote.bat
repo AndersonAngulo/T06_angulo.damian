6dda559ceaf93f6d60ba100bfbcb56235e6101a5
AndersonAngulo <56948485+andersonangulo@users.noreply.github.com>
2019-11-17 13:42:15 -0500

    Se crearon diez condicionales dobles de damian

---
+++ lote.bat
@@ -0,0 +1,123 @@
+python angulo\condicionales dobles\ejer1.py "matematica" 15 15 15 15
+python angulo\condicionales dobles\ejer1.py "historia" 12 15 16 14
+python angulo\condicionales dobles\ejer1.py "lenguaje" 20 14 18 11
+
+python angulo\condicionales dobles\ejer2.py  1.5 1.5 1.5 1.5
+python angulo\condicionales dobles\ejer2.py  1.2 1.5 1.6 1.4
+python angulo\condicionales dobles\ejer2.py  2.0 1.4 1.8 1.1
+
+python angulo\condicionales dobles\ejer3.py 15 15
+python angulo\condicionales dobles\ejer3.py 16 14
+python angulo\condicionales dobles\ejer3.py 18 11
+
+python angulo\condicionales dobles\ejer4.py 15 15 5
+python angulo\condicionales dobles\ejer4.py 15 16 4
+python angulo\condicionales dobles\ejer4.py 14 18 3
+
+python angulo\condicionales dobles\ejer5.py 15 8
+python angulo\condicionales dobles\ejer5.py 16 4
+python angulo\condicionales dobles\ejer5.py 18 6
+
+python angulo\condicionales dobles\ejer6.py 8
+python angulo\condicionales dobles\ejer6.py 4
+python angulo\condicionales dobles\ejer6.py 7
+
+python angulo\condicionales dobles\ejer7.py 15 15
+python angulo\condicionales dobles\ejer7.py 16 14
+python angulo\condicionales dobles\ejer7.py 18 11
+
+python angulo\condicionales dobles\ejer8.py 15 15
+python angulo\condicionales dobles\ejer8.py 16 14
+python angulo\condicionales dobles\ejer8.py 18 11
+
+python angulo\condicionales dobles\ejer9.py 20 35
+python angulo\condicionales dobles\ejer9.py 70 24
+python angulo\condicionales dobles\ejer9.py 83 41
+
+python angulo\condicionales dobles\ejer10.py 50 15
+python angulo\condicionales dobles\ejer10.py 60 50
+python angulo\condicionales dobles\ejer10.py 80.5 14
+
+
+
+python angulo\condicionales multiples\ejer1.py "matematica" 15 15 15 15
+python angulo\condicionales multiples\ejer1.py "historia" 12 15 16 14
+python angulo\condicionales multiples\ejer1.py "lenguaje" 20 14 18 11
+
+python angulo\condicionales multiples\ejer2.py  1.5 1.5 1.5 1.5
+python angulo\condicionales multiples\ejer2.py  1.2 1.5 1.6 1.4
+python angulo\condicionales multiples\ejer2.py  2.0 1.4 1.8 1.1
+
+python angulo\condicionales multiples\ejer3.py 15 15
+python angulo\condicionales multiples\ejer3.py 16 14
+python angulo\condicionales multiples\ejer3.py 18 11
+
+python angulo\condicionales multiples\ejer4.py 15 15 5
+python angulo\condicionales multiples\ejer4.py 15 16 4
+python angulo\condicionales multiples\ejer4.py 14 18 3
+
+python angulo\condicionales multiples\ejer5.py 15 8
+python angulo\condicionales multiples\ejer5.py 16 4
+python angulo\condicionales multiples\ejer5.py 18 6
+
+python angulo\condicionales multiples\ejer6.py 8
+python angulo\condicionales multiples\ejer6.py 4
+python angulo\condicionales multiples\ejer6.py 7
+
+python angulo\condicionales multiples\ejer7.py 15 15
+python angulo\condicionales multiples\ejer7.py 16 14
+python angulo\condicionales multiples\ejer7.py 18 11
+
+python angulo\condicionales multiples\ejer8.py 15 15
+python angulo\condicionales multiples\ejer8.py 16 14
+python angulo\condicionales multiples\ejer8.py 18 11
+
+python angulo\condicionales multiples\ejer9.py 20 35
+python angulo\condicionales multiples\ejer9.py 70 24
+python angulo\condicionales multiples\ejer9.py 83 41
+
+python angulo\condicionales multiples\ejer10.py 50 15
+python angulo\condicionales multiples\ejer10.py 60 50
+python angulo\condicionales multiples\ejer10.py 80.5 14
+
+
+
+python angulo\condicionales simples\ejer1.py "geometria" 15 15 15 15
+python angulo\condicionales simples\ejer1.py "logica" 12 15 16 14
+python angulo\condicionales simples\ejer1.py "filosofia" 20 14 18 11
+
+python angulo\condicionales simples\ejer2.py  1.5 1.5 1.5 1.5
+python angulo\condicionales simples\ejer2.py  1.2 1.5 1.6 1.4
+python angulo\condicionales simples\ejer2.py  2.0 1.4 1.8 1.1
+
+python angulo\condicionales simples\ejer3.py 1.5 15
+python angulo\condicionales simples\ejer3.py 16 1.4
+python angulo\condicionales simples\ejer3.py 18 11
+
+python angulo\condicionales simples\ejer4.py 15 15 5
+python angulo\condicionales simples\ejer4.py 15 1.6 4
+python angulo\condicionales simples\ejer4.py 14 18 3
+
+python angulo\condicionales simples\ejer5.py 1.5 8
+python angulo\condicionales simples\ejer5.py 16 4
+python angulo\condicionales simples\ejer5.py 18 6
+
+python angulo\condicionales simples\ejer6.py 8
+python angulo\condicionales simples\ejer6.py 4
+python angulo\condicionales simples\ejer6.py 7
+
+python angulo\condicionales simples\ejer7.py 1.5 1.5
+python angulo\condicionales simples\ejer7.py 1.6 14
+python angulo\condicionales simples\ejer7.py 18 1.1
+
+python angulo\condicionales simples\ejer8.py 15 1.5
+python angulo\condicionales simples\ejer8.py 1.6 14
+python angulo\condicionales simples\ejer8.py 18 11
+
+python angulo\condicionales simples\ejer9.py 20 3.5
+python angulo\condicionales simples\ejer9.py 7.0 24
+python angulo\condicionales simples\ejer9.py 83 41
+
+python angulo\condicionales simples\ejer10.py 5.9 1.5
+python angulo\condicionales simples\ejer10.py 6.8 5.0
+python angulo\condicionales simples\ejer10.py 80.5 1.4
